c4b219866429bd4cac211d122bf9440db8b0e964
Tripathy Divyyajyoti Senapati <divyajyoti.ts@gmail.com>
2022-03-30 03:33:33 +0530

    comments

--- ctftime-api-testing.py
+++ ctftime-api-testing.py
@@ -7,6 +7,7 @@ params = {'limit': '100', 'start': '1646006400', 'finish': '1648588662'}
 apires = r.get(url, headers=headers, params=params)
 print(apires.json())
 
+#credit for solving that api error ; nullctf's implementation of ctftime
 #todo
 #implement db or yml parser
 #autobot to parse all events on ctftime
